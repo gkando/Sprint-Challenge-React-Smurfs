84a244b066fb208e9cea03a9d4d11b898674727e
George Kando <gkando@gmail.com>
2019-06-14 11:59:00 -0500

    final commit

--- village/src/App.js
+++ village/src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import Smurf from './components/Smurf';
+import NavBar from './components/NavBar'
 
 import axios from 'axios'
 
@@ -28,18 +31,27 @@ class App extends Component {
     this.getSmurfs()
 }
 
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-  // Notice what your map function is looping over and returning inside of Smurfs.
-  // You'll need to make sure you have the right properties on state and pass them down to props.
   render() {
     return (
       <div className="App">
         <div className='village'>
           <img src={require("./images/house1.png")} alt="smurf-house" /> 
         </div>
-        <SmurfForm refresh={this.getSmurfs()}/>
-        <Smurfs smurfs={this.state.smurfs} />
-        <div id="talkbubble"> </div>
+
+        <NavBar />
+
+        <Route
+              exact 
+              path='/'
+              render={props => <Smurfs smurfs={this.state.smurfs} />}
+          />
+
+            <Route
+              exact
+              path="/:id"
+              component={SmurfForm}
+              refresh={this.getSmurfs()}
+             />
       </div>
     );
   }
